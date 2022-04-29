a58e3d91b7de3ded14462f387779059b900599c8
Kelho <Kelho0812@hotmail.com>
2022-04-29 22:10:00 +0100

    created author & pages headers for the card

--- JS/script.js
+++ JS/script.js
@@ -9,22 +9,33 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 function printBooks() {
+    
     for (let i = (myLibrary.length-1); i < myLibrary.length; i++) {
         const bookCard = document.createElement("div");
         bookCard.classList.add("bookCards");
         mainScreen.appendChild(bookCard);
 
-        const cardTitle = document.createElement("div");
+        const cardTitle = document.createElement("h3");
         cardTitle.classList.add("bookTitle");
         const cardAuthor = document.createElement("div");
         cardAuthor.classList.add("bookAuthor");
         const cardPages = document.createElement("div");
         cardPages.classList.add("bookPages");
+
+
+        const authorHeader = document.createElement("h4");
+        authorHeader.classList.add("author");
+        const pagesHeader = document.createElement("h4");
+        pagesHeader.classList.add("pages");
         
         bookCard.appendChild(cardTitle);
         bookCard.appendChild(cardAuthor);
         bookCard.appendChild(cardPages);
+        bookCard.appendChild(authorHeader);
+        bookCard.appendChild(pagesHeader);
 
+        authorHeader.textContent = "Author: ";
+        pagesHeader.textContent = "Pages: ";
         cardTitle.textContent = myLibrary[i].title;
         cardAuthor.textContent = myLibrary[i].author;
         cardPages.textContent = myLibrary[i].pages;
